35034d7cd1a4bc46970d0fc35adb44616ce3bc8e
Nikki Thean <nikki.thean@gmail.com>
2014-02-27 20:50:38 -0500

    Update readme

--- editor.rb
+++ editor.rb
@@ -8,26 +8,27 @@ class Editor
   def command(line)
     case line
     when /^i (\d+) (\d+)$/
-      @image = Image.new($1.to_i, $2.to_i)
+      @image = Image.new($2.to_i, $1.to_i)
     when /^([cs])$/
       $1 == "c" ? image.clear : draw
     when /^([lf]) (\d+) (\d+) ([a-z])$/
-      $1 == "l" ? image.color($2.to_i, $3.to_i, $4.upcase) : image.fill($2.to_i, $3.to_i, $4.upcase)
+      $1 == "l" ? image.color($3.to_i, $2.to_i, $4.upcase) : image.fill($3.to_i, $2.to_i, $4.upcase)
     when /^([vh]) (\d+) (\d+) (\d+) ([a-z])$/
        $1 == "v" ? image.vertical($2.to_i, $3.to_i, $4.to_i, $5.upcase) : image.horizontal($2.to_i, $3.to_i, $4.to_i, $5.upcase)
     when /^x$/
       exit
     else
       puts "Sorry, I didn't understand your input."
-      command(Editor.input)
     end
   end
 
   def draw
+    puts
     image.pixels.to_a.each do |row|
       row.each {|pixel| print pixel + " "}
       puts
     end
+    puts
   end
 
   def self.input
--- try.rb
+++
@@ -1,5 +0,0 @@
-require './image'
-require './editor'
-require './matrix'
-
-Editor.run
